27967f055c3fed87bc85cd2301ef670adc690784
heraldofortuna <heraldofortuna@gmail.com>
2021-03-23 18:40:21 -0500

    I deleted useless files in the app

--- get-that-home/src/index.tsx
+++ get-that-home/src/index.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-
-import './index.css'
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import App from './App'
 
 ReactDOM.render(
