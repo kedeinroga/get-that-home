eb028ecb8b1689692d7b192ece0a571c21075a37
heraldofortuna <heraldofortuna@gmail.com>
2021-03-23 18:34:44 -0500

    I added dependencies like @emotion/react, @emotion/styled, react-router-dom, faker and react-icons

--- get-that-home/src/App.tsx
+++ get-that-home/src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { Navbar } from './components/Navbar'
-import { About } from './pages/About'
-import { Home } from './pages/Home'
+import { Navbar } from './components/Navbar';
+import { About } from './pages/About';
+import { Home } from './pages/Home';
 
 const App: React.FC = () => {
   return (
