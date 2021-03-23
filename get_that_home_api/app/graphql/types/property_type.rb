317e6abaa73cc21a323a1f91b5b67e951717e61a
AlvaroJTorres <mangetrone@gmail.com>
2021-03-23 18:03:57 -0500

    Used graphql to make a query to create a Property

--- get_that_home_api/app/graphql/types/property_type.rb
+++ get_that_home_api/app/graphql/types/property_type.rb
@@ -1,17 +1,17 @@
 module Types
   class PropertyType < Types::BaseObject
     field :id, ID, null: false
-    field :operation_type, Integer, null: true
-    field :address, String, null: true
-    field :rent, Integer, null: true
+    field :operation_type, Integer, null: false
+    field :address, String, null: false
+    field :rent, Integer, null: false
     field :maintanance, Integer, null: true
-    field :property_type, Integer, null: true
+    field :property_type, Integer, null: false
     field :bedrooms, Integer, null: true
     field :bathrooms, Integer, null: true
-    field :area, Float, null: true
-    field :pets, Boolean, null: true
+    field :area, Float, null: false
+    field :pets, Boolean, null: false
     field :about, String, null: true
-    field :photos, String, null: true
+    field :photos, String, null: false
     field :user_id, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
