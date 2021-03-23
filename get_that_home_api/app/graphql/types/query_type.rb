d87fa27c95b6ed1750990a5962038970b73e4a5e
AlvaroJTorres <mangetrone@gmail.com>
2021-03-23 18:13:49 -0500

    Added query to list a property by Id using graphql

--- get_that_home_api/app/graphql/types/query_type.rb
+++ get_that_home_api/app/graphql/types/query_type.rb
@@ -4,5 +4,6 @@ module Types
     # They will be entry points for queries on your schema.
 
     field :fetch_properties, resolver: Queries::FetchProperties
+    field :fetch_property, resolver: Queries::FetchProperty
   end
 end
