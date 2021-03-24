b05c870bd4f7d0395181e2a76008018cc6e8d029
heraldofortuna <heraldofortuna@gmail.com>
2021-03-24 11:00:05 -0500

    Default styles for button component were added

--- get-that-home/src/components/Button.tsx
+++ get-that-home/src/components/Button.tsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled'
 import React, { ReactNode } from 'react'
+import { RiUserLine, RiArrowDownSLine } from 'react-icons/ri'
+import { colors, TextButton } from '../ui'
 
 interface IProps {
   children: ReactNode
@@ -7,11 +9,33 @@ interface IProps {
 }
 
 const StyledButton = styled.button`
-  background-color: black;
+  background-color: ${colors.pink};
+  color: ${colors.white};
+  border: ${colors.pink};
+  border-radius: 16px;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 8px 16px;
+
+  & > p {
+    color: ${colors.white};
+    margin: 0 12px;
+  }
+
+  & > svg {
+    fill: ${colors.white};
+    font-size: 20px;
+  }
 `
 
 export default function Button({ children }: IProps) {
   return (
-    <StyledButton>{ children }</StyledButton>
-  );
+    <StyledButton>
+      <RiUserLine />
+      <TextButton>{children}</TextButton>
+      <RiArrowDownSLine />
+    </StyledButton>
+  )
 }
