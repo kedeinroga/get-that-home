d491f9db9bd7ec4ad743295c5a1f55b547b7697a
kedeinroga <keybroga@gmail.com>
2021-03-24 10:05:12 -0500

    modularization of mutations properties

--- get_that_home_api/app/graphql/types/input/property_input_type.rb
+++ get_that_home_api/app/graphql/types/input/property_input_type.rb
@@ -1,15 +1,15 @@
 module Types
   module Input
     class PropertyInputType < Types::BaseInputObject
-      argument :operation_type, Integer, required: true
+      argument :operation_type, Integer, required: false
       argument :address, String, required: true
       argument :rent, Integer, required: true
       argument :maintanance, Integer, required: false
-      argument :property_type, Integer, required: true
+      argument :property_type, Integer, required: false
       argument :bedrooms, Integer, required: false
       argument :bathrooms, Integer, required: false
       argument :area, Float, required: true
-      argument :pets, Boolean, required: true
+      argument :pets, Boolean, required: false
       argument :about, String, required: false
       argument :photos, String, required: true
       argument :user_id, Integer, required: true
