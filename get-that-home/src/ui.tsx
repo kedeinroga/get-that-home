25803e792ce700e55d9e2a84f8d9de03cb69163c
heraldofortuna <heraldofortuna@gmail.com>
2021-03-23 23:23:29 -0500

    I created a UI file with different colors

---
+++ get-that-home/src/ui.tsx
@@ -0,0 +1,15 @@
+const colors = {
+  pink: "#F48FB1",
+  lightPink: "#FFC1E3",
+  darkPink: "#BF5F82",
+  shallowPink: "rgba(244, 143, 177, 0.15)",
+  gray: "#616161",
+  lightGray: "#8E8E8E",
+  darkGray: "#373737",
+  shallowGray: "rgba(97, 97, 97, 0.15)",
+  background: "#F5F5F6",
+  backgroundLight: "#E1E2E1",
+  white: "#FFFFFF",
+}
+
+export default { colors };
