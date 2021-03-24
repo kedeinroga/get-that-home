01e4513b15df5b2fce4e99336710b0de8f14e0e2
kedeinroga <keybroga@gmail.com>
2021-03-24 10:07:46 -0500

    modularization of queries properties

--- get_that_home_api/app/graphql/queries/fetch_properties.rb
+++
@@ -1,9 +0,0 @@
-module Queries
-  class FetchProperties < Queries::BaseQuery
-    type [Types::PropertyType], null: false
-
-    def resolve
-      Property.all.order(created_at: :desc)
-    end
-  end
-end
---
+++ get_that_home_api/app/graphql/queries/properties/fetch_property.rb
@@ -0,0 +1,17 @@
+module Queries
+  module Properties
+    class FetchProperty < Queries::BaseQuery
+      type Types::PropertyType, null: false
+      argument :id, ID, required: true
+
+      def resolve(id:)
+        Property.find(id)
+      rescue ActiveRecord::RecordNotFound => _e
+        GraphQL::ExecutionError.new('Note does not exist.')
+      rescue ActiveRecord::RecordInvalid => e
+        GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
+          " #{e.record.errors.full_messages.join(', ')}")
+      end
+    end
+  end
+end
