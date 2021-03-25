d28f1f08b89d962162a5277cc3a984b4c5af82b2
heraldofortuna <heraldofortuna@gmail.com>
2021-03-25 10:45:44 -0500

    HTML structure of Card component was created

--- get-that-home/src/components/Card.tsx
+++ get-that-home/src/components/Card.tsx
@@ -1,13 +1,61 @@
+import React from 'react'
 import styled from '@emotion/styled'
+import { RiCoinsLine } from 'react-icons/ri'
+import { TextBody2 } from '../ui'
+
+interface IPropsCL {
+  type?: string
+}
 
 const StyledCard = styled.div`
-
+  background-color: yellow;
+  height: 100px;
+  width: 100px;
 `
 
+const StyledCardLabel = styled.div`
+  background-color: black;
+`
+
+function CardLabel(props: IPropsCL) {
+  const { type } = props
+
+  switch(type) { 
+    case 'rental': {
+      return (
+        <StyledCardLabel>
+          <RiCoinsLine />
+          <TextBody2>
+            For Rental
+          </TextBody2>
+        </StyledCardLabel>
+      )
+    } 
+    case 'sale': { 
+      return (
+        <StyledCardLabel>
+          <RiCoinsLine />
+          <TextBody2>
+            For Rental
+          </TextBody2>
+        </StyledCardLabel> 
+      )
+    } 
+    default: { 
+       break; 
+    } 
+ } 
+}
+
 export default function Card() {
+
   return (
     <StyledCard>
-      Card
+      <CardLabel />
     </StyledCard>
   )
+}
+
+CardLabel.defaultProps = {
+  type: 'rental'
 }
