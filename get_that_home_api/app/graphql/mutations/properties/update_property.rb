ab4b0230acd4731c9498361a6ff02e31ec0478c6
AlvaroJTorres <mangetrone@gmail.com>
2021-03-24 20:43:44 -0500

    Property can only be updated by it's owner and added custom IncorrectUser error when the curent user it's not the owner

--- get_that_home_api/app/graphql/mutations/properties/update_property.rb
+++ get_that_home_api/app/graphql/mutations/properties/update_property.rb
@@ -1,5 +1,7 @@
+class IncorrectUser < StandardError; end
+
 module Mutations
-  module Properties 
+  module Properties
     class UpdateProperty < Mutations::BaseMutation
       argument :id, ID, required: true
 
@@ -9,12 +11,18 @@ module Mutations
 
       def resolve(id:, params:)
         property_params = Hash params
+        property_params[:user_id] = context[:current_user].id
 
         begin
           property = Property.find(id)
-          property.update!(property_params)
-
-          { property: property }
+          if context[:current_user].id == property.user_id
+            property.update!(property_params)
+            { property: property }
+          else
+            raise IncorrectUser, "You don't own this property"
+          end
+        rescue IncorrectUser => e
+          GraphQL::ExecutionError.new(e.message)
         rescue ActiveRecord::RecordInvalid => e
           GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
             " #{e.record.errors.full_messages.join(', ')}")
