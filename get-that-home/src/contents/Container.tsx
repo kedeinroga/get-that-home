58648690819567cffa5028adb3cc430a00f40610
heraldofortuna <heraldofortuna@gmail.com>
2021-03-25 02:12:47 -0500

    Container function was created inside contents portfolio

---
+++ get-that-home/src/contents/Container.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled'
+
+const StyledContainer = styled.div`
+  width: 95%;
+  margin: 0 auto;
+`
+
+export default function Container() {
+  return (
+    <StyledContainer>
+    </StyledContainer>
+  )
+}
