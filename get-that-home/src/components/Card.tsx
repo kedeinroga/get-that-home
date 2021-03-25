bcbcfc5cbcededcdeef8ec5ab4056ee84c38749f
heraldofortuna <heraldofortuna@gmail.com>
2021-03-25 10:09:46 -0500

    I initialized card component

---
+++ get-that-home/src/components/Card.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled'
+
+const StyledCard = styled.div`
+
+`
+
+export default function Card() {
+  return (
+    <StyledCard>
+      Card
+    </StyledCard>
+  )
+}
--- get-that-home/src/pages/Home.tsx
+++ get-that-home/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react'
 import Header from '../components/Header'
+import Card from '../components/Card'
 
 export const Home: React.FC = () => {
   return (
     <Fragment>
       <Header />
+      <Card />
     </Fragment>
   )
 }
