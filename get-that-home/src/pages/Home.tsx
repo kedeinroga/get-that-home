b04166424cc39a64ffacb79e57a8d18238b6cc6f
heraldofortuna <heraldofortuna@gmail.com>
2021-03-26 17:48:31 -0500

    I created a small component called HomeTitle in Home screen

--- get-that-home/src/pages/Home.tsx
+++ get-that-home/src/pages/Home.tsx
@@ -1,12 +1,25 @@
 import React, { Fragment } from 'react'
+import styled from '@emotion/styled'
 import Header from '../components/Header'
 import Card from '../components/Card'
 import SelectContent from '../contents/SelectContent'
+
+const StyledHomeTitle = styled.div`
+  background-color: yellow;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`
 
 export const Home: React.FC = () => {
   return (
     <Fragment>
       <Header />
+      <StyledHomeTitle>
+        <h2>Meet your new Home</h2>
+        <h5>The easiest way to find where you belong</h5>
+      </StyledHomeTitle>
       <Card
         landlord
         type="sale"
