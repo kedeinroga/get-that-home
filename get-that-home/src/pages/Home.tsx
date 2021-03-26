af3402098fd48de1614afa52660b3b2785a39bb1
heraldofortuna <heraldofortuna@gmail.com>
2021-03-26 18:03:50 -0500

    Styled Hero section was implemented in Home screen

--- get-that-home/src/pages/Home.tsx
+++ get-that-home/src/pages/Home.tsx
@@ -1,25 +1,39 @@
 import React, { Fragment } from 'react'
 import styled from '@emotion/styled'
+import { colors } from '../ui'
 import Header from '../components/Header'
 import Card from '../components/Card'
 import SelectContent from '../contents/SelectContent'
+import illustrationHero from '../assets/illustration-1.svg'
+
+const StyledHero = styled.section`
+  height: 600px;
+  width: 100%;
+  background-image: url(${illustrationHero});
+`
 
 const StyledHomeTitle = styled.div`
-  background-color: yellow;
+  background-color: rgba(40, 40, 40, 0.25);
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 8px;
+
+  & > h5 {
+    color: ${colors.gray};
+  }
 `
 
 export const Home: React.FC = () => {
   return (
     <Fragment>
       <Header />
-      <StyledHomeTitle>
-        <h2>Meet your new Home</h2>
-        <h5>The easiest way to find where you belong</h5>
-      </StyledHomeTitle>
+      <StyledHero>
+        <StyledHomeTitle>
+          <h2>Meet your new Home</h2>
+          <h5>The easiest way to find where you belong</h5>
+        </StyledHomeTitle>
+      </StyledHero>
       <Card
         landlord
         type="sale"
