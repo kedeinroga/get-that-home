e36e22304afe0efc46a7c559bff803662782020a
heraldofortuna <heraldofortuna@gmail.com>
2021-03-26 01:17:49 -0500

    I added animated components to select

--- get-that-home/src/contents/SelectContent.tsx
+++ get-that-home/src/contents/SelectContent.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Select from 'react-select'
+import styled from '@emotion/styled'
+import makeAnimated from 'react-select/animated'
 
 const options = [
   { value: 'chocolate', label: 'Chocolate' },
@@ -7,6 +9,19 @@ const options = [
   { value: 'vanilla', label: 'Vanilla' },
 ]
 
+const StyledSelect = styled.div``
+
+const animatedComponents = makeAnimated()
+
 export default function SelectContent() {
-  return <Select options={options} />
+  return (
+    <StyledSelect>
+      <Select
+        closeMenuOnSelect={false}
+        components={animatedComponents}
+        isMulti
+        options={options}
+      />
+    </StyledSelect>
+  )
 }
