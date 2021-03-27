9ece5f9694ca39089e90dc4da4d6dbed94cab1c7
heraldofortuna <heraldofortuna@gmail.com>
2021-03-27 01:21:10 -0500

    Footer section was fixed. I created a file for footer with props and styles too

--- get-that-home/src/pages/Home.tsx
+++ get-that-home/src/pages/Home.tsx
@@ -6,6 +6,7 @@ import Card from '../components/Card'
 import IllustrationHero from '../assets/illustration-1.svg'
 import Button from '../components/Button'
 import TeamCard from '../components/TeamCard'
+import Footer from '../components/Footer'
 
 const StyledHero = styled.section`
   height: 600px;
@@ -92,33 +93,6 @@ const StyledTeam = styled.section`
   }
 `
 
-const StyledFooter = styled.footer`
-  background-color: ${colors.background};
-  border-top: 1px solid ${colors.darkPink};
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 4px;
-  padding: 16px 32px;
-
-  & > div {
-    width: 75%;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  & > section {
-    width: 75%;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-  }
-`
-
 export const Home: React.FC = () => {
   return (
     <Fragment>
@@ -196,21 +170,7 @@ export const Home: React.FC = () => {
           />
         </ul>
       </StyledTeam>
-      <StyledFooter>
-        <div>
-          <TextSubtitle2>© 2021 - Find That Home</TextSubtitle2>
-          <TextSubtitle2>Source Code</TextSubtitle2>
-          <TextSubtitle2>Codeable - Cohort 3 Final Project</TextSubtitle2>
-        </div>
-        <section>
-          <div>
-            <TextSubtitle2>Ruby on Rails REST API</TextSubtitle2>
-          </div>
-          <div>
-            <TextSubtitle2>React Responsive SPA</TextSubtitle2>
-          </div>
-        </section>
-      </StyledFooter>
+      <Footer landing />
     </Fragment>
   )
 }
