a9a1c2200cce9a22f37cca560bd0ae4ba8111d34
heraldofortuna <heraldofortuna@gmail.com>
2021-03-29 16:43:43 -0500

    I modified the App function: I added routes and some pages like Landing and Login

--- get-that-home/src/App.js
+++ get-that-home/src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div className="App"></div>;
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import styled from '@emotion/styled'
+
+import { Landing } from './pages/Landing'
+import { Login } from './pages/Login'
+
+const Container = styled.div`
+  background-color: red;
+`
+
+var App = function () {
+  return (
+    <BrowserRouter>
+      <Container>
+        <Switch>
+          <Route path="/" component={Landing} exact />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
+  )
 }
 
 export default App;
