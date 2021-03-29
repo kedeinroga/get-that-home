a9f5a031f7b4d664dddc861d9aebf6af1ba7fd72
heraldofortuna <heraldofortuna@gmail.com>
2021-03-29 16:22:35 -0500

    I deleted useless ffiles

--- get-that-home/src/index.js
+++ get-that-home/src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { ApolloProvider } from "@apollo/client";
 import client from "./client";
 import Login from "./components/auth/Login";
@@ -18,8 +16,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
