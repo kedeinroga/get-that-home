c884a55352256c4419050be204c4c18046b9c20a
heraldofortuna <heraldofortuna@gmail.com>
2021-03-30 00:04:51 -0500

    Hero section was initialized

--- get-that-home/src/pages/Landing.js
+++ get-that-home/src/pages/Landing.js
@@ -1,12 +1,36 @@
+import styled from "@emotion/styled";
 import Header from "../components/Header";
 import Card from "../components/Card";
+import ImageHero from "../assets/image-hero.svg";
+
+const StyledLanding = styled.div``;
+
+const StyledLandingHero = styled.section`
+  width: 100%;
+  height: 600px;
+  background-image: url(${ImageHero});
+  background-position: center center;
+  background-size: cover;
+
+  & > div {
+    display: grid;
+    place-items: center;
+    gap: 8px;
+  }
+`;
 
 export function Landing() {
   return (
-    <div>
+    <StyledLanding>
       <Header />
+      <StyledLandingHero>
+        <div>
+          <h2>Meet your new Home</h2>
+          <h5>The easiest way to find where you belong</h5>
+        </div>
+      </StyledLandingHero>
       <Card />
       <Card landlord type="sale" />
-    </div>
+    </StyledLanding>
   );
 }
